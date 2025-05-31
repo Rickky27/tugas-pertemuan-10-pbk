0819149fb5ebbf27dc293b857e82541912ad50ea
Rickky27 <bddragondri@gmail.com>
2025-05-31 19:43:53 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
   <router-view />
+
+  <div>
+    <components />
+    <DataManager />
+  </div>
 </template>
+
+<script setup>
+  import components from './components/components.vue';
+  import DataManager from './components/DataManager.vue';
+</script>
